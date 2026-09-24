996b62c0f79bbd9b7f520efd8f28387cf769b2c7
elsakezia <elsakezia90@gmail.com>
2026-09-24 11:26:30 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,54 +1,30 @@
-const readline = require('readline');
+let totalHarga = 0;
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+let jumlahPengunjung = parseInt(prompt("Masukkan jumlah pengunjung:"));
 
-  return new Promise((resolve) => rl.question(question, resolve));
-}
-
-  let jumlahPengunjung, usia, i;
-  let hargaTiket, totalHarga, hargaAkhir, diskon;
-
-  totalHarga = 0;
-
-  jumlahPengunjung = parseInt(await ask('Masukkan jumlah pengunjung: '), 10);
-
-  for (i = 1; i <= jumlahPengunjung; i++) {
-    usia = parseInt(await ask(`Masukkan usia pengunjung ke-${i}: `), 10);
-
+for (let i = 1; i <= jumlahPengunjung; i++) {
+    let usia = parseInt(prompt("Masukkan usia pengunjung ke-" + i + ":"));
+    
+    let hargaTiket = 0;
     if (usia < 5) {
-      hargaTiket = 0;
-    } else {
-      if (usia <= 12) {
+        hargaTiket = 0;
+    } else if (usia <= 12) {
         hargaTiket = 5;
-      } else {
-        if (usia <= 59) {
-          hargaTiket = 10;
-        } else {
-          hargaTiket = 7;
-        }
-      }
+    } else if (usia <= 59) {
+        hargaTiket = 10;
+    } else {
+        hargaTiket = 7;
     }
-
-    totalHarga = totalHarga + hargaTiket;
-  }
-
-  if (jumlahPengunjung >= 5) {
-    diskon = totalHarga * 0.10;
-  } else {
-    diskon = 0;
-  }
-
-  hargaAkhir = totalHarga - diskon;
-
-  console.log('Jumlah Pengunjung: ' + jumlahPengunjung);
-  console.log('Total Harga: ' + totalHarga);
-  console.log('Diskon: ' + diskon);
-  console.log('Harga: ' + hargaAkhir);
-
-  rl.close();
+    
+    totalHarga += hargaTiket;
 }
 
-main();
+let diskon = (jumlahPengunjung >= 5) ? (totalHarga * 0.10) : 0;
+let hargaAkhir = totalHarga - diskon;
+
+alert(
+    "Jumlah Pengunjung: " + jumlahPengunjung + "\n" +
+    "Total Harga: " + totalHarga + "\n" +
+    "Diskon: " + diskon + "\n" +
+    "Harga Akhir: " + hargaAkhir
+);
